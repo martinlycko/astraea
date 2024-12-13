c7b7e19658709c82e0aa97c94be94f8328e69e06
mlycko <martin@mlycko.com>
2024-12-13 18:54:00 +0000

    Clean up and set up

--- build.zig
+++ build.zig
@@ -15,12 +15,16 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const scramble = b.dependency("scramble", .{ .target = target }).module("scramble");
+
     const exe = b.addExecutable(.{
         .name = "astraea",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    exe.root_module.addImport("scramble", scramble);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
--- src/main.zig
+++ src/main.zig
@@ -1,49 +1,7 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
-const outw = std.io.getStdOut().writer();
-const stdin = std.io.getStdIn().reader();
+const scramble = @import("scramble");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-
-    var input: [10]u8 = undefined;
-    try outw.print("Enter:\n", .{});
-    _ = try stdin.readUntilDelimiter(&input, '\n');
-    try outw.print("Your input was: {s}\n", .{input});
-
-    try outw.print("Enter input 2:\n", .{});
-    const input2 = try stdin.readUntilDelimiterAlloc(std.heap.page_allocator, '\n', 8192);
-    defer std.heap.page_allocator.free(input2);
-    try outw.print("Your input was: {s}\n", .{input2});
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
+    _ = scramble.WordCounter();
+    std.debug.print("test", .{});
 }
