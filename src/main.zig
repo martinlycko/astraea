b1fcb4d1b614adf37a17d3141007fde31e9335cb
mlycko <martin@mlycko.com>
2024-12-15 18:05:55 +0000

    Single file analysed

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
 const scramble = @import("scramble");
 
+const allocator = std.heap.page_allocator;
 pub fn main() !void {
-    _ = scramble.WordCounter();
-    std.debug.print("test", .{});
+    var dir = try std.fs.cwd().openDir("data", .{});
+    defer dir.close();
+
+    var file = try dir.openFile("Business Intelligence Analyst - Diamond4Jobs.txt", .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(allocator, 10000);
+    defer allocator.free(content);
+
+    var WordsInJobDescriptions = scramble.WordCounter().init(allocator);
+    defer WordsInJobDescriptions.deinit();
+
+    try WordsInJobDescriptions.countwords(content);
+
+    try WordsInJobDescriptions.printorderedwordlist();
 }
