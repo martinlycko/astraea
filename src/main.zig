ed86ed02921b16c0186a7a6908feaed5ab0124ae
mlycko <martin@mlycko.com>
2024-12-16 19:54:23 +0000

    Read multiple files

--- src/main.zig
+++ src/main.zig
@@ -3,19 +3,24 @@ const scramble = @import("scramble");
 
 const allocator = std.heap.page_allocator;
 pub fn main() !void {
-    var dir = try std.fs.cwd().openDir("data", .{});
-    defer dir.close();
-
-    var file = try dir.openFile("Business Intelligence Analyst - Diamond4Jobs.txt", .{});
-    defer file.close();
-
-    const content = try file.readToEndAlloc(allocator, 10000);
-    defer allocator.free(content);
-
     var WordsInJobDescriptions = scramble.WordCounter().init(allocator);
     defer WordsInJobDescriptions.deinit();
 
-    try WordsInJobDescriptions.countwords(content);
+    var dir = try std.fs.cwd().openDir("data", .{ .iterate = true });
+    defer dir.close();
+
+    var iter = dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .file) {
+            var file = try dir.openFile("Business Intelligence Analyst - Diamond4Jobs.txt", .{});
+            defer file.close();
+
+            const content = try file.readToEndAlloc(allocator, 10000);
+            defer allocator.free(content);
+
+            try WordsInJobDescriptions.countwords(content);
+        }
+    }
 
     try WordsInJobDescriptions.printorderedwordlist();
 }
